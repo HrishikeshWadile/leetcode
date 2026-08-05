290721cb9a1c9a27aedf8772850078aa94591a2b
Hrishikesh Wadile <143935445+HrishikeshWadile@users.noreply.github.com>
2026-08-05 20:06:04 +0530

    Time: 1 ms (70.52%), Space: 43.1 MB (9.91%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if (num == 0) return "0";
+
+        boolean flag = false;
+        if (num < 0) {
+            flag = true;
+            num = -num;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.insert(0, num % 7);
+            num /= 7;
+        }
+
+        if (flag) {
+            sb.insert(0, "-");
+        }
+
+        return sb.toString();
+    }
+}
