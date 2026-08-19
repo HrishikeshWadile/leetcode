516d4f6feec8530eea6805f5a27d4c4afb077af1
Hrishikesh Wadile <143935445+HrishikeshWadile@users.noreply.github.com>
2026-08-19 09:48:22 +0530

    Time: 29 ms (41.32%), Space: 52.3 MB (65.53%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -2,10 +2,12 @@ class Solution {
 
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
 
-        Arrays.sort(reservedSeats,
-            Comparator.comparingInt((int[] a) -> a[0])
-                      .thenComparingInt(a -> a[1])
-        );
+        Arrays.sort(reservedSeats, (a, b) -> {
+            if (a[0] != b[0])
+                return Integer.compare(a[0], b[0]);
+
+            return Integer.compare(a[1], b[1]);
+        });
 
         int ans = 0;
         int i = 0;
