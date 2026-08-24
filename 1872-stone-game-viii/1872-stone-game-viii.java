525f83a0e36ec645b9701c063b5482da02e43d0f
Hrishikesh Wadile <143935445+HrishikeshWadile@users.noreply.github.com>
2026-08-24 08:23:04 +0530

    Time: 3 ms (100%), Space: 83.4 MB (84.26%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+
+        int sum = 0;
+        for (int x : stones) {
+            sum += x;
+        }
+
+        int diff = sum;
+
+        for (int i = n - 2; i >= 1; i--) {
+            sum -= stones[i + 1];
+            diff = Math.max(diff, sum - diff);
+        }
+
+        return diff;
+    }
+}
